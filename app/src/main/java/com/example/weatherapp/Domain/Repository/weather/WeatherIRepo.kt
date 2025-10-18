b6f89e11c8fbd0611abd323c92716a49b030ff38
Magar Aniruddha Maruti <aniruddhamaruti2005@gmail.com>
2025-10-19 00:06:26 +0530

    Delete app directory

--- app/src/main/java/com/example/weatherapp/Domain/Repository/weather/WeatherIRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weatherapp.Domain.Repository.weather
-
-import com.example.weatherapp.Data.remote.WeatherDto
-
-interface WeatherIRepo {
-    suspend fun getweather(city:String): WeatherDto
-}
